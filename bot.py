2d6304d7e4935bbdf18df07a78e45b01b9522d63
mallusrgreatv2 <freedomgamingyt@gmail.com>
2021-07-04 17:16:07 +0530

    select thingy

--- bot.py
+++ bot.py
@@ -2,8 +2,14 @@ import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
+from discord_slash import SlashCommand
 load_dotenv()
 client = commands.Bot(command_prefix="??", description="made for zey nd mlsrgrt to learn python")
+slash = SlashCommand(client, sync_commands=True)
+client.slash = slash
+@slash.slash(name = "ping", guild_ids=[853316413649190912], description="Bot's latency")
+async def ping(ctx):
+    await ctx.send("Pong! {}".format(str(round(client.latency))+"ms"))
 
 for filename in os.listdir('cogs'):
     if filename.endswith(".py"):
--- cogs/tests.py
+++ cogs/tests.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import Cog
 from discord_components import Button
+from discord_components.component import Select, SelectOption
 class Tests(Cog):
     def __init__(self, client) -> None:
         self.client: Cog = client
@@ -16,8 +17,25 @@ class Tests(Cog):
             Button(label="Click", style = 1),
             Button(label="Disabled", disabled=True)
         ])
-        interaction = await self.client.wait_for("button_click", check=lambda i: i.component.label == 'Click')
-        interaction.reply("hey")
+        interaction = await self.client.wait_for("button_click", check=lambda i: i.component.label.startswith("Click"))
+        await interaction.respond("hey")
+
+    @commands.command()
+    async def select(self, ctx: commands.Context):
+        await ctx.send('Here, select whatever you want', components = [
+            Select(
+                placeholder="Here!!!",
+                options=[SelectOption(label="one", value="1"), SelectOption(label="two", value="2")]
+            ),
+            Select(
+                placeholder="this is disabled lol",
+                options=[SelectOption(label="hackar?", value="hacks")],
+                disabled=True
+            )
+        ])
+
+        interaction = await self.client.wait_for("select_option")
+        await interaction.respond(content=f"{interaction.component[0].label} is it!")
 
 
 def setup(client):
