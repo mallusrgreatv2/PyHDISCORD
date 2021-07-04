0962895a5df02067268fc1da201f509b98c0479b
mallusrgreatv2 <freedomgamingyt@gmail.com>
2021-07-04 02:32:46 -0700

    bot activity

--- cogs/events.py
+++ cogs/events.py
@@ -3,12 +3,13 @@ from discord.ext import commands
 from discord_components import *
 class Events(commands.Cog):
     def __init__(self, client) -> None:
-        self.client: commands.Cog = client
+        self.client: commands.Bot = client
     
     @commands.Cog.listener()
     async def on_ready(self):
         print("---Events Cog Ready ||| Bot Ready---")
         DiscordComponents(self.client)
+        await self.client.change_presence(activity=discord.Game("games"))
 
 def setup(client):
     client.add_cog(Events(client))
