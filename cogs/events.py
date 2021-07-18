e414976441cbdb3a57b2c545ab164810bebe2e4b
mallusrgreatv2 <69511006+mallusrgreatv2@users.noreply.github.com>
2021-07-18 17:54:10 +0530

    Update events.py

--- cogs/events.py
+++ cogs/events.py
@@ -52,7 +52,8 @@ class Events(commands.Cog):
         
         else:
             await ctx.send(f"{err.__class__.__name__}: {err}")
-
+    
+    # @commands.Cog.listener
     async def on_guild_join(self, guild):
         with open('dicts/prefixes.json', 'r') as f:
             prefixes = json.load(f)
@@ -62,7 +63,7 @@ class Events(commands.Cog):
         with open('dicts/prefixes.json', 'w') as f:
             json.dump(prefixes, f, indent=4)
 
-    @commands.Cog.listener()
+    # @commands.Cog.listener()
     async def on_guild_remove(self, guild):
         with open('dicts/prefixes.json', 'r') as f:
             prefixes = json.load(f)
